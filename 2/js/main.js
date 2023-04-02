b7e5a6ed71363dd163608ed5bd3f6b45e1c6aa3b
Keks <keks@htmlacademy.ru>
2023-04-02 20:40:45 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,15 @@
+function randInt(min,max) {
+  if (max < min) {
+    throw Error('Incorrect input. Min should be less or equal than max');
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random()*(max - min + 1) + min);
+}
+
+function stringLengthChecker(string, maxLength) {
+  const length = string.length;
+  return length <= maxLength;
+}
+
+
