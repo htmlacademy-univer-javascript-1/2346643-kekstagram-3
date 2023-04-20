e7ce059547b0e9e4bb1104977ae919890bda7db5
Keks <keks@htmlacademy.ru>
2023-04-20 17:22:34 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,6 @@
+import {randInt} from './util'; // eslint-disable-line no-unused-vars
+import {stringLengthChecker} from './util'; // eslint-disable-line no-unused-vars
+import {photosDataGenerator} from './data'; // eslint-disable-line no-unused-vars
+
+const photosNumber = 25;
+const photosData = photosDataGenerator(photosNumber); // eslint-disable-line no-unused-vars
